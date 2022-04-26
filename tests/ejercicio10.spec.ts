7dbdc298bb1657c88fb32831c71e88fc47985ce6
Jonay <alu0101347141@ull.edu.es>
2022-04-26 22:37:59 +0100

    Inicializado repositorio

---
+++ tests/ejercicio10.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Prueba} from '../src/ejercicio10';
+
+describe('Prueba', () => {
+  it('Existe una clase de prueba', () => {
+    expect(Prueba).to.exist;
+  });
+});
